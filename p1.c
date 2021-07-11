bc924d8979ca65aaba86fe7bc11bd1cdcc23d8fd
Karthik <karthik_kr@hotmail.com>
2021-07-10 21:26:36 -0700

    Added desc for fork() system call

--- p1.c
+++ p1.c
@@ -1,3 +1,11 @@
+/* The purpose of this simple program is to illustrate few things - 
+   1) The system call fork() creates a new child process which is almost a copy of its calling process.
+      The fork() is not an exact copy, as it comes to life as if it has called fork on itself.
+      Means, the new process starts executing from the line - int rc = fork() and not from the main itself.
+      This is an important distinction for the OS as the line int rc = fork() for the child process returns 0 while for 
+      the parent process it returns the child PID.
+   2) The non deterministic output -  Assuming that the program runs on a single core, we are able to unable to say which
+      process runs first. It depends on the scheduling policy both in single core and multi core CUPs.*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
