e98104e6fdf980a43dc7a9dd743ffce592e4f208
Karthik <karthik_kr@hotmail.com>
2021-07-13 22:13:34 -0700

    Add all exec variant details

--- Assignments/Question4.c
+++ Assignments/Question4.c
@@ -2,7 +2,14 @@
 Write a program that calls fork() and then calls some form of exec() to run the program /bin/ls.
 See if you can try all of the variants of exec(), including (on Linux) execl(), execle(), execlp(), execv(), execvp(), and execvpe().
 Why do you think there are so many variants of the same basic call?
-Answers -  - Karthik Kolathumani
+Answers - The exec family of function replaces the current process with new process image. The stack, heap and other data segments are reinitialized
+          The exec() has a family of functions that follow the 'exec' prefix.
+          -l execl(), execlp(), execle() - int execl(const char *pathname, const char *arg, ..., (char *) NULL);
+                                           Takes in argument as a list of pointers to strings that are either binary executable or a script that starts with #!interpreter [optional-arg]
+          -v execv(), execvp(), execvpe()- Takes in argument of array of one or more null terminated strings.
+          -e execle(), execvpe() -  The environment of the nre process image is specified by the 'envp' argument -  int execvpe(const char *file, char *const argv[], char *const envp[]);
+          -p execlp(), execvp(), execvpe() - These functions duplicate the actions of the shell in searching for an executable file if the specified filename does not contain the '/' character.
+                                             The file is sought in the colon - separated list of directory pathnames specified in the PATH environment variable.
  */
 #include<stdio.h>
 #include<stdlib.h>
